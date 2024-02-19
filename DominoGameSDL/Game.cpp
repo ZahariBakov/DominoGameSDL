c0a172310a1e270193501326989165c04e78d52b
ZahariBakov <zaharibakov@gmail.com>
2024-02-19 20:35:24 +0200

    Add "MENU" and "PASS" buttons, and checking if they are pressed.

--- DominoGameSDL/Game.cpp
+++ DominoGameSDL/Game.cpp
@@ -94,6 +94,12 @@ bool Game::ttf_init() {
 	SDL_QueryTexture(newTex, 0, 0, &tw, &th);
 	newRect = { 10, 10, tw, th };
 
+	SDL_QueryTexture(menuTex, 0, 0, &tw, &th);
+	menuRect = { 10, 40, tw, th };
+
+	SDL_QueryTexture(passTex, 0, 0, &tw, &th);
+	passRect = { 10, 70, tw, th };
+
 	SDL_FreeSurface(tempSurfaceText);
 	TTF_CloseFont(font1);
 
@@ -108,9 +114,14 @@ void Game::render() {
 
 	if (!Game::isGame) {
 		TextureManager::Instance()->drawTexture("welcome", 0, 0, ww, wh, renderer, SDL_FLIP_NONE);
+		
 	}
 
 	SDL_RenderCopy(renderer, newTex, NULL, &newRect);
+	SDL_RenderCopy(renderer, menuTex, NULL, &menuRect);
+	SDL_RenderCopy(renderer, passTex, NULL, &passRect);
+
+	
 
 	SDL_RenderPresent(renderer);
 }
@@ -134,7 +145,7 @@ void Game::handleEvents() {
 			case SDL_MOUSEBUTTONUP: {
 				if (event.button.button == SDL_BUTTON_LEFT) {
 					SDL_GetMouseState(&msx, &msy);
-					Game::isClicked(mouseDownY, mouseDownY, msx, msy);
+					Game::isClicked(mouseDownX, mouseDownY, msx, msy);
 				}
 			}; break;
 
@@ -154,15 +165,38 @@ void Game::isClicked(int xDown, int yDown, int xUp, int yUp) {
 	int ww, wh;
 	SDL_GetWindowSize(window, &ww, &wh);
 
-	int BtnX = 10;
-	int BtnY = 10;
-	int BtnW = 155;
-	int BtnH = 23;
+	int btnX = 10;
+	int btnY = 14;
+	int btnW = 150;
+	int btnH = 23;
 
-	if ((xDown > BtnX && xDown < (BtnX + BtnW)) && (xUp > BtnX && xUp < (BtnX + BtnW)) &&
-		(yDown > BtnY && yDown < (BtnY + BtnH)) && (yUp > BtnY && yUp < (BtnY + BtnH))) {
+	if ((xDown > btnX && xDown < (btnX + btnW)) && (xUp > btnX && xUp < (btnX + btnW)) &&
+		(yDown > btnY && yDown < (btnY + btnH)) && (yUp > btnY && yUp < (btnY + btnH))) {
 		Game::isGame = true;
 		std::cout << "NEW GAME Button is clicked!" << std::endl;
+
+		return;
+	}
+
+	btnY = 40;
+	btnW = 75;
+
+	if ((xDown > btnX && xDown < (btnX + btnW)) && (xUp > btnX && xUp < (btnX + btnW)) &&
+		(yDown > btnY && yDown < (btnY + btnH)) && (yUp > btnY && yUp < (btnY + btnH))) {
+		std::cout << "MENU Button is clicked!" << std::endl;
+
+		return;
+	}
+
+
+	btnY = 75;
+	btnH = 20;
+
+	if ((xDown > btnX && xDown < (btnX + btnW)) && (xUp > btnX && xUp < (btnX + btnW)) &&
+		(yDown > btnY && yDown < (btnY + btnH)) && (yUp > btnY && yUp < (btnY + btnH))) {
+		std::cout << "PASS Button is clicked!" << std::endl;
+
+		return;
 	}
 }
 
